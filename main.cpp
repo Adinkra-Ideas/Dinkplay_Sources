6596ae5940afd3a2537d8edc8578283ffa905855
Adinkra-Ideas <musicjeejo@gmail.com>
2025-01-17 05:19:49 +0100

    update

--- main.cpp
+++
@@ -1,8 +0,0 @@
-extern int _main(int argc, char *argv[]);
-
-int main(int argc, char *argv[])
-{
-    return _main(argc, argv);
-}
-
-// /Users/daniel/Documents/Dinkplayobj/build/Qt_6_8_1_for_iOS/qrc_qml.cpp
